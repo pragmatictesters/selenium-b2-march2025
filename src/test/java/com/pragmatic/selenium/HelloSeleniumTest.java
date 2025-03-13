51fa4051ecc40e53b48898344011dbb7010d238b
Janesh Kodikara <janesh@pragmatictesters.com>
2025-03-13 09:25:14 +0530

    Refactor initial test: maximize browser, clear text fields, and add assertion message.

--- src/test/java/com/pragmatic/selenium/HelloSeleniumTest.java
+++ src/test/java/com/pragmatic/selenium/HelloSeleniumTest.java
@@ -2,6 +2,7 @@ package com.pragmatic.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,20 +13,25 @@ public class HelloSeleniumTest {
     @Test
     public void testHelloSelenium() {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
         //Navigate to the sauce login page
         driver.get("https://www.saucedemo.com/");
 
         //Type username
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        WebElement txtUsername = driver.findElement(By.id("user-name"));
+        txtUsername.clear();
+        txtUsername.sendKeys("standard_user");
         //Type password
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        WebElement txtPassword = driver.findElement(By.id("password"));
+        txtPassword.clear();
+        txtPassword.sendKeys("secret_sauce");
         //Click login button
         driver.findElement(By.id("login-button")).click();
 
         //Verify the error message
-        String errorMessage = driver.findElement(By.cssSelector(".title")).getText();
-        Assert.assertEquals(errorMessage, "Products");
+        String actualTitle = driver.findElement(By.cssSelector(".title")).getText();
+        Assert.assertEquals(actualTitle, "Products", "Login failed - Page title mismatch.");
         driver.quit();
     }
 
